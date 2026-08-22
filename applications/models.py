68b3848a10cb452b3858c917a02b5b61a6cd8b45
Bobby Walters <jubale1328@zoho.com>
2026-08-22 08:25:54 -0700

    base.html template not working

--- applications/models.py
+++ applications/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+    posted_date = models.DateTimeField(default=timezone.now) # Date job application was saved by user
+    application_date = models.DateTimeField(blank=True, null=True) # Date application was submitted to company
+    applied = models.CharField(max_length=50) # True/False - Should be changed to a drop down or check box
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # JobSlide user who added application
+    company = models.CharField(max_length=250) # Company offering position
+    position = models.CharField(max_length=250) # Company position of interest
+    min_salary = models.CharField(max_length=200, default="") # Minimum Salary offered
+    max_salary = models.CharField(max_length=200, default="") # Maxmium Salary offered
+
+
+def publish(self):
+    self.posted_date = timezone.now()
+    self.save()
+
+def __str__(self):
+    return self.position + ' - ' + self.company
--- mysite/settings.py
+++ mysite/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-r7p5tmh$1wk14b-9%vqt0f-q)n-cy1)d&i_=2=5h=u@$al3x-o'
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-r7p5tmh$1wk14b-9%vqt0f-q)n-cy1)d&i_=2=5h=u@$al3x-o'
+#SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -33,13 +33,13 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'applications',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'applications',
 ]
 
 MIDDLEWARE = [
