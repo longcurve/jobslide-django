a820da8f168d05bc72a860f1ab6f3fbe4156eb83
Bobby Walters <jubale1328@zoho.com>
2026-08-29 17:28:59 -0700

    Updating allowed hosts to allow pythonanywhere

--- applications/models.py
+++ applications/models.py
@@ -5,14 +5,15 @@ from django.utils import timezone
 # Create your models here.
 
 class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # JobSlide user who added application
     posted_date = models.DateTimeField(default=timezone.now) # Date job application was saved by user
     application_date = models.DateTimeField(blank=True, null=True) # Date application was submitted to company
-    applied = models.CharField(max_length=50) # True/False - Should be changed to a drop down or check box
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # JobSlide user who added application
-    company = models.CharField(max_length=250) # Company offering position
+    status = models.CharField(max_length=50) # True/False - Should be changed to a drop down or check box
     position = models.CharField(max_length=250) # Company position of interest
     min_salary = models.CharField(max_length=200, default="") # Minimum Salary offered
     max_salary = models.CharField(max_length=200, default="") # Maxmium Salary offered
+    company = models.CharField(max_length=250) # Company offering position
+    location = models.CharField(max_length=250) # Location of company
 
 
 def publish(self):
--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-r7p5tmh$1wk14b-9%vqt0f-q)n-cy1)d&i_=2=5h=u@$al3x-o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*.pythonanywhere.com']
 
 
 # Application definition
